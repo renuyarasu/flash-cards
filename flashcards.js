a30692e22c3183f6a1d120ef7491d450676ac021
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-05-21 14:48:28 +0530

    flashcards: Project

--- flashcards.js
+++ flashcards.js
@@ -1,1 +1,34 @@
-.
+document.getElementById("btnSave").addEventListener("click", saveCard);
+let cards = new Array();
+
+function saveCard() {
+  let frontContent = document.getElementById("frontCard").value;
+  let backContent = document.getElementById("backCard").value;
+  let card = { front: frontContent, back: backContent };
+  cards.push(card);
+  console.log(cards);
+  clearUI();
+  numCardsOut();
+  storeCards();
+}
+storeCards = () => {
+  let serializedCards = JSON.stringify(cards);
+  localforage
+    .setItem("flashcards", serializedCards)
+    .then(function () {
+      return localforage.getItem("key");
+    })
+    .then(function (valus) {
+      alert("Saved");
+    })
+    .catch(function (err) {
+      console.log("erroe: " + err);
+    });
+};
+numCardsOut = () => {
+  document.getElementById("numCards").innerHTML = cards.length;
+};
+clearUI = () => {
+  document.getElementById("frontCard").value = "";
+  document.getElementById("backCard").value = "";
+};
